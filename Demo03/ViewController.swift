2478d3311f34fd560cb56ee6a770b0b87038f5cb
MiaoChaoran <a20131105796@admindeiMac-08.local>
2016-05-23 20:08:09 +0800

    足球计分器
    带图片

---
+++ Demo03/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo03
+//
+//  Created by 20131105796MCR on 16/5/12.
+//  Copyright © 2016年 20131105796MCR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
